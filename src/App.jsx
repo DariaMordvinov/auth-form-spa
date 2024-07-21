c2a34746d95bea595d78d2466f572b0c78e9d40b
Dasha <daria.sidelnikova.2020@gmail.com>
2024-07-21 14:52:50 -0400

    boiler plate

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Auth from "./containers/Auth";
+
+function App() {
+
+  return (
+    <div>
+      <Auth />
+    </div>
+  )
+}
+
+export default App
---
+++ src/containers/Auth.jsx
@@ -0,0 +1,12 @@
+import styles from "./auth.module.css";
+
+const Auth = ({ }) => {
+
+    return (
+        <div>
+            <button className={styles.button}>hey</button>
+        </div>
+    )
+}
+
+export default Auth;
