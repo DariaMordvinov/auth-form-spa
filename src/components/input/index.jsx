9e7b62e813ad9736caf7979052350373324cb4dc
Dasha <daria.sidelnikova.2020@gmail.com>
2024-07-22 19:26:50 -0400

    label click

--- src/components/input/index.jsx
+++ src/components/input/index.jsx
@@ -1,16 +1,29 @@
 import styles from "./input.module.css";
+import { useRef } from "react";
 
 export const Input = ({ label, type, value, setValue }) => {
+    const inputRef = useRef(null);
+
     const handleChange = (e) => {
         setValue(e.target.value);
     }
 
+    const handleLabelClick = () => {
+        if (document.activeElement !== inputRef.current) {
+            inputRef.current.focus();
+        }
+    };
+
     return (
         <div className={styles.input}>
-            <label className={`${styles.label} ${value?.length > 0 ? styles.filled : ""}`} htmlFor={type}>
+            <label
+                onClick={handleLabelClick}
+                className={`${styles.label} ${value?.length > 0 ? styles.filled : ""}`}
+                htmlFor={type}>
                 {label}
             </label>
             <input
+                ref={inputRef}
                 id={label}
                 value={value}
                 onChange={(e) => handleChange(e)}
